5770066c68d29be0a7e8fc6cca191c386b24afd8
parres <parres@LapHP-Parres.ds.iteso.mx>
2017-01-24 18:27:09 -0600

    Initial

---
+++ routes/wines.js
@@ -0,0 +1,62 @@
+/* 
+ ***************************************************
+ *     APLICACIONES Y SERVICIOS EN LA NUBE         *
+ *                   ITESO                         *
+ *                                                 * 
+ *    Actividad 1: Diseño de un WebService         *
+ *    Codigo Base: Alvaro Parres (parres@iteso.mx) * 
+ *                                                 * 
+ *    Alumno: <COMPLETAR SU NOMBRE>                *
+ *    Exp: <Numero_de_Expediente>                  *
+ *                                                 *
+ ***************************************************
+ *                                                 *
+ * Instrucciones: Complete el codigo basado en     * 
+ * las indicaciones descritas en el documento      *
+ *                                                 *
+ ***************************************************
+ */
+
+var Wine = require('../models/wine');
+
+//Phase 1
+exports.findAll = function(req, res) {
+    
+    console.log('All Wines Request');
+    //Modified the res.send code to return two JSON Objects 
+    res.send({"id":"ID", "name":"nombre", "description":"DESCRIPCION"});
+
+    /*
+     *Put Phase2 Code here.
+     * 
+
+};
+
+exports.findById = function(req, res) {
+
+    console.log('ID: '+req.params.id+' Wine Request');
+    //Modified the res.send line to send a JSON Object with the requested ID. 
+    res.send({"id":"ID", "name":"nombre", "description":"DESCRIPCION"});    
+
+    /*
+     * The next code is for Phase 2.
+     * 
+     * Modified this method to return one specific wine from collection.
+     * You have to use the method findById which has the next syntaxis:
+     *      findById(id, callback(err, result))
+     *   
+     */                            
+
+};
+
+/*
+* The next code is for Phase 2.
+* 
+*  Create the methods:
+*    addWine
+*    deleteWine
+*    updateWine
+*    
+*  Some hints about this tree method are in HomeWork document.
+*/
+          
